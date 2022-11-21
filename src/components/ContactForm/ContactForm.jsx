021501ecbbac795d1defbc72cf5f95a18bb7cb39
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-11-21 18:14:34 +0200

    Update contactForm to redux

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,40 +1,26 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import css from './ContactForm.module.css'
+import { useDispatch, useSelector } from 'react-redux';
 import { BsPhoneFill } from "react-icons/bs";
 import { FaUserPlus } from "react-icons/fa";
+import css from './ContactForm.module.css'
+import { addContact } from '../../redux/contactsSlice';
 
-const ContactForm = ({ onSubmit }) => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
 
-  // const onChangeInputName = event => {
-  //   setName(event.target.value);
-  // };
-  // const onChangeInputNumber = event => {
-  //   setNumber(event.target.value);
-  // };
+const ContactForm = () => {
+  const contacts = useSelector(state => state.contacts);
+  const dispatch = useDispatch();
 
-const onChangeInput = event => {
-  const { name, value } = event.target;
-  switch (name) {
-    case 'name':
-      setName(value);
-      break;
-    case 'number':
-      setNumber(value);
-      break;
-
-      default:
-        console.warn(`Field ${name} not found.`);
-  }
-}
-
-  const handleSubmit = event => {
+  function handleSubmit(event){
     event.preventDefault();
-    onSubmit({name, number});
-    setName('');
-    setNumber('');
+    const field = event.target;
+    const name = field.name.value;
+    const number = field.number.value;
+    if (contacts.find(
+      contact => contact.name.toLowerCase().trim() === name.toLowerCase().trim()
+    )) {
+      return window.alert(`${name} is already in contacts !`);
+    }
+    dispatch(addContact(name, number));
+    field.reset();
   };
 
 return ( 
@@ -46,8 +32,6 @@ return (
       pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
       title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
       required
-      onChange={onChangeInput}
-      value={name}
       /> 
         </label>
         <label className={css.label}>Number<BsPhoneFill size={20} className={css.icon}/>:
@@ -57,17 +41,11 @@ return (
       pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
       title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
       required
-      onChange={onChangeInput}
-      value={number}
     /> 
         </label>
     <button type="submit" className={css.btn}><FaUserPlus size={20} className={css.btn_icon}/>Add contact</button>
     </form>
     );
 }
-
-ContactForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
 
 export default ContactForm;
