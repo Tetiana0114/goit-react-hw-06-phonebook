0f06881173eaf607edbd2aac63facb89aa956906
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-11-21 16:20:10 +0200

    Add contactsSlice

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { nanoid } from 'nanoid';
+
+const contactsInitialState = [];
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(name, number) {
+        return {
+            payload: {
+                name: name, 
+                number: number,
+                id: nanoid(),
+          },
+        };
+      },
+    },
+    deleteContact(state, action) {
+        const index = state.findIndex(contact => contact.id === action.payload);
+        state.splice(index, 1);
+      },
+    },
+  },
+);
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
